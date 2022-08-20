99b3fdb2726cec896ab7a10f64279de13ea46ba4
john89ram <john89ram@gmail.com>
2022-08-20 10:07:31 -0500

    TIY 2.8&2.9

---
+++ chapter_2_Variables&SimpleDataTypes/TryItYourself/2_8.py
@@ -0,0 +1,5 @@
+#Write a * , / , + , - , that will result with 8.
+print(8*1)
+print(4+4)
+print(8/1)
+print(16-8)
---
+++ chapter_2_Variables&SimpleDataTypes/TryItYourself/2_9.py
@@ -0,0 +1,6 @@
+#Create a variable to represent your favorite number. Write a message to reveal your number and print the message
+
+fav_num = 24
+message = f"My favorite number is {fav_num}!"
+
+print(message)
