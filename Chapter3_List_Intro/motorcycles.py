95d857fcb1541a1e98009b2723755ee66b057ecb
john89ram <john89ram@gmail.com>
2022-08-21 16:49:07 -0500

    stop

--- Chapter3_List_Intro/motorcycles.py
+++ Chapter3_List_Intro/motorcycles.py
@@ -2,10 +2,19 @@
 motorcycles = ['honda','yamaha','suzuki']
 print(motorcycles)
 
-#Modifying a list
+#Modifying an element to a list
 motorcycles[0] = 'ducati'
 print(motorcycles)
 
-#Adding to a list
+#Appending will add to an element to the end of the list.
 motorcycles.append('honda')
 print(motorcycles)
+
+#You can so start with an empty list and append items as the program continues.
+fav_foods = []
+print(fav_foods)
+
+fav_foods.append('burger')
+fav_foods.append('pizza')
+fav_foods.append('pasta')
+print(fav_foods)
