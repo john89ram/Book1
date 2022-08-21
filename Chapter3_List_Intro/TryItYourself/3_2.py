ba94dd4635d25a11226ab2924da14d0527992c49
john89ram <john89ram@gmail.com>
2022-08-21 15:58:17 -0500

    tiy ch3

---
+++ Chapter3_List_Intro/TryItYourself/3_2.py
@@ -0,0 +1,5 @@
+#Create a list of friends and family and create a message to that person. 
+names = ['jonathan', 'ashley', 'daymian', 'micheal', 'andreah']
+
+message = f"Hello {names[0].title()}, remember you are an amazing person. \n\tAlways keep working hard and good things will come."
+print(message)
---
+++ Chapter3_List_Intro/TryItYourself/3_3.py
@@ -0,0 +1,7 @@
+#Make your own list and create a message.
+car_makes = ['honda','mazda','kia','ford','chevy']
+vehicles = ['boat','motorcycle','car','truck'] 
+
+message = f"I have always wanted to own a {car_makes[0].title()} {vehicles[2]}."
+
+print (message)
