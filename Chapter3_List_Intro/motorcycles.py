9bef483690dfea696c90e235b98a9ac82735d9cc
john89ram <john89ram@gmail.com>
2022-08-21 16:31:08 -0500

    3.3 updates

---
+++ Chapter3_List_Intro/motorcycles.py
@@ -0,0 +1,11 @@
+#Practicing to modify list add/delete/change 
+motorcycles = ['honda','yamaha','suzuki']
+print(motorcycles)
+
+#Modifying a list
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+#Adding to a list
+motorcycles.append('honda')
+print(motorcycles)
