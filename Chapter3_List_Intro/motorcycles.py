ffe11e45069d1f8cbfd7de8ffbe78bfe56bdcad2
john89ram <john89ram@gmail.com>
2022-08-22 16:55:04 -0500

    Ch3 updates

--- Chapter3_List_Intro/motorcycles.py
+++ Chapter3_List_Intro/motorcycles.py
@@ -18,3 +18,13 @@ fav_foods.append('burger')
 fav_foods.append('pizza')
 fav_foods.append('pasta')
 print(fav_foods)
+
+#Inserting elements into a list. Inserting an element will shift all others to the right of the index placement.
+fav_foods.insert(1, 'TACOS')
+print(fav_foods)
+
+#Removing elements by index, you must know the position of the element in order to delete by this method.
+    #Elements that are deleted can not be accessed afterwards
+del fav_foods[1]
+print(fav_foods)
+
