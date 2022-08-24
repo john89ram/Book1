8ac2ca2ac069c473785f4053ab2d087b119e6bc3
john89ram <john89ram@gmail.com>
2022-08-23 20:44:54 -0500

    3.9

---
+++ Chapter3_List_Intro/TryItYourself/3_10.py
@@ -0,0 +1,6 @@
+#Every function - Create your own custom list while doing so use everything you learned in this chapter at leaset once.
+    # Create list, use functions; 
+    # Print list
+    # Access an element within list
+    # Use the index within list to create a message
+     
