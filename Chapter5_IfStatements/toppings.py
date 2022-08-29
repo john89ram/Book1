003587a9bbf60a88924c86a761ddaff1b1d5b03b
john89ram <john89ram@gmail.com>
2022-08-29 16:47:14 -0500

    updates

--- Chapter5_IfStatements/toppings.py
+++ Chapter5_IfStatements/toppings.py
@@ -5,3 +5,9 @@ requested_toppings = 'mushrooms'
 
 if requested_toppings != 'anchovies':
     print('Hold the anchovies!')
+
+# You can also request if an element is "in" a list
+
+requested_toppings = ['mushrooms','onions','pineapple']
+
+# In the Python environment you can call "'mushrooms' in requested_toppings" and 
