7ccbea4c2455e79b51356cff293eb94d9c5f609f
john89ram <john89ram@gmail.com>
2022-09-06 16:59:14 -0500

    Update after sickness

--- Chapter5_IfStatements/toppings.py
+++ Chapter5_IfStatements/toppings.py
@@ -11,3 +11,28 @@ if requested_toppings != 'anchovies':
 requested_toppings = ['mushrooms','onions','pineapple']
 
 # In the Python environment you can call "'mushrooms' in requested_toppings" and Python will return "True"
+
+#------------------------------------------*** Testing conditions cont. *****************************************
+requested_toppings = ['mushrooms','extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza")
+# This is testing for exact conditions when creating this users pizza.
+#-
+# This code will not work like the last one and will stop once the first statement calls back True
+requested_toppings = ['mushrooms','extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+elif 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+elif 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza")
