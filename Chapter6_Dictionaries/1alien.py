8a53a0866adb3f1502163e62121c5474f5844be6
john89ram <john89ram@gmail.com>
2022-09-17 01:04:59 -0500

    Ch6 updates

---
+++ Chapter6_Dictionaries/1alien.py
@@ -0,0 +1,11 @@
+# A simple dictionary holding information of an aliens color and point value.
+alien_0 = {'color': 'green', 'points': 5}
+
+# Now lets print the elements that this variable holds.
+print(alien_0['color'])
+print(alien_0['points'])
+
+#--------------------- *** Example of how to call a key-value pair ***
+
+new_points = alien_0['points']
+print(f"You just earned {new_points} points!")
