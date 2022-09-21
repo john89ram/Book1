6712b45ebb7ac50e1e952ccd4efd074e4aef0d46
john89ram <john89ram@gmail.com>
2022-09-20 22:17:37 -0500

    .

--- Chapter6_Dictionaries/1alien.py
+++ Chapter6_Dictionaries/1alien.py
@@ -37,3 +37,24 @@ print(f"The alien is now {alien_0['color']}!")
 alien_0 = {'x_position':0, 'y_position':25, 'speed':'medium'}
 print(f"Original position: {alien_0['x_position']}")
 
+    # Move the alien to the right
+    # Determine how far to move the alien based on its current speed
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # 'fast' alien 
+    x_increment = 3
+
+# Lets add the aliens speed to its current position to make it move
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print(f"New position: {alien_0['x_position']}")
+
+# Deleting values from dictionaries
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
