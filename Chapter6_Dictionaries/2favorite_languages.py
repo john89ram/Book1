857bb4b57494f54dd50772fb02ea1959619ea1dd
john89ram <john89ram@gmail.com>
2022-09-21 15:51:24 -0500

    .

--- Chapter6_Dictionaries/2favorite_languages.py
+++ Chapter6_Dictionaries/2favorite_languages.py
@@ -1,4 +1,6 @@
 # Dictionary of similar objects
+    # You can create all types of dictionaries (One item with a lot of attributes or even one subject with a lot of ppl.)
+    # Create a pool of ppl and their favorite programming language
 
 favorite_languages = {
     'jen': 'python',
@@ -6,6 +8,6 @@ favorite_languages = {
     'edward': 'ruby',
     'phil': "python"
 }
-
+# Now lets print out the favorite language of someone within the poll
 language = favorite_languages['sarah'].title()
 print(f"Sarah's favorite language is {language}.")
---
+++ Chapter6_Dictionaries/3alien_no_points.py
@@ -0,0 +1,6 @@
+# Using the get() to access values
+    # You will get an error when calling on keys that do not exist in a dictionary.
+alien_0 = {'color': 'green','speed': 'slow'}
+print(alien_0['points'])
+
+#
