f43869158a03cf07c4f183f349a2822ddc00530b
john89ram <john89ram@gmail.com>
2022-10-05 23:48:08 -0500

    .

---
+++ Chapter6_Dictionaries/7-many_users.py
@@ -0,0 +1,25 @@
+# Dictionary in a dictionary
+    # Creating a nested dictionary with dictionaries
+
+users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+    },
+
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+    },
+    
+}
+
+for username, user_info in users.items():
+    print(f"\nUsername: {username}")
+    full_name = f"{user_info['first']} {user_info['last']}"
+    location = user_info['location']
+
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tLocation: {location.title()}")
---
+++ Chapter6_Dictionaries/TIY/6_7.py
@@ -0,0 +1,12 @@
+# People - Take the what you have learned from exercise 7 as well as the TIY 6.1 and make 2 more dictionaries entries of other people. 
+    # Store all 3 people in a list called "people".
+    # Lastly loop through the list and print all the information that was learned about them.
+
+main_character = {'first_name': 'ashley', 'last_name': 'ramirez', 'age': 31, 'city': 'leander'}
+main_character2 = {'first_name': 'jonathan', 'last_name': 'ramirez', 'age': 33, 'city': 'austin'}
+main_character3 = {'first_name': 'micheal', 'last_name': 'ramirez', 'age': 9, 'city': 'cedar park'}
+
+people = [main_character, main_character2, main_character3]
+
+for name, user_info in people.items():
+    
