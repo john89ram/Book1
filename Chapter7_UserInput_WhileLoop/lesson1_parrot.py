7b4bffd2e172441b32ef1c9ba8f46495d096350b
john89ram <john89ram@gmail.com>
2022-10-13 13:53:21 -0500

    Ch7 start

---
+++ Chapter7_UserInput_WhileLoop/lesson1_parrot.py
@@ -0,0 +1,8 @@
+# Working with the input() function. 
+    # The input() function will pause the program and wait for the user to enter something
+    # Then it will assign it to a variable to use at another time.
+
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
+
+# Here the user input is connected to the "message" variable and then can be used to call when printing it
