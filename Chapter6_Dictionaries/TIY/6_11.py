8cd25c42658151833a9134968b9277c0bd6e0311
john89ram <john89ram@gmail.com>
2022-10-12 19:12:14 -0500

    6.11

---
+++ Chapter6_Dictionaries/TIY/6_11.py
@@ -0,0 +1,25 @@
+# Cities - Make a dictionaries called "cities". Use the name of 3 cities as keys. 
+    # Create a nested dictionary inside for the values of the cities.
+    # Iniside input info over these cities (country, population, and an interesting fact)
+    # Print each citie and the facts that discovered for each.
+    
+cities = {
+    'ontario': {
+        'country': 'canada',
+        'population': 14_570_000,
+        'fact': ' known for Parliament Hill’s Victorian architecture and the National Gallery',
+        }, 
+
+    'austin': {
+        'country': 'america',
+        'population': 965_872,
+        'fact': 'known as the live music capital of the USA',
+        },
+
+    'hot springs': {
+        'country': 'america',
+        'population': 38_697,
+        'fact': 'known for naturally heated springs',
+        },
+}
+
