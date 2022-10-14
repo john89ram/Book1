af20e4aa696f3b031940b0dbb486e8c1fce1eca6
john89ram <john89ram@gmail.com>
2022-10-14 16:44:47 -0500

    lesson 5

---
+++ Chapter7_UserInput_WhileLoop/lesson5_counting.py
@@ -0,0 +1,12 @@
+# Intro to while loops - While loops will continue to run until a condition is true
+    # Lets dip our toes into the this function by creating a program to run until it counts to a certain number
+
+current_num = 1
+while current_num <= 5:
+    print(current_num)
+    current_num += 1
+
+# Here we establish a while condition to run until the "current_num" is 5
+    # The next line will inform us what the current number is
+    # The last line will add 1 to the current number and it will start over again
+    # Once the current_num = 5, the program will stop.
