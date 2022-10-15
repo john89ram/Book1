dd39c15d9c890356d2c2ef360671f4b9361ceb75
john89ram <john89ram@gmail.com>
2022-10-15 08:41:51 -0500

    5.5 done

--- Chapter7_UserInput_WhileLoop/lesson5.5_parrot.py
+++ Chapter7_UserInput_WhileLoop/lesson5.5_parrot.py
@@ -1,3 +1,12 @@
 # Lets like lesson 1 we are going to implement what we just learned
+    # Instead of the program ending after the input from the user create a while loop
+    # to stop when the user types "quit"
 
- 
+prompt = "\nTell me something and I will repeat it back to you."
+prompt += "\nEnter 'quit' to end the program."
+
+message = ""
+
+while message != 'quit':
+    message = input(prompt)
+    print(f"\n{message}")
