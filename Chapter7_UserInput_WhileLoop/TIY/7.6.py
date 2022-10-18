a6c3123e768686952706b438f96c70e2f5ff8654
john89ram <john89ram@gmail.com>
2022-10-18 14:11:17 -0500

    7.6 challenge(not complete)

---
+++ Chapter7_UserInput_WhileLoop/TIY/7.6.py
@@ -0,0 +1,4 @@
+# Three Exits - Write a different version of TIY 7.4 or 7.5 but include the following requirements:
+    # Use a conditional test in a while statement to stop the loop
+    # Use an active variable to control how long the loop runs
+    # Use a break statement to exit the loop when the user enters 'quit'
