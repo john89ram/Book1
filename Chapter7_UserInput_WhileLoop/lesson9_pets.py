a059ace63302d0d115f54a5b15f63be59d7b0d78
john89ram <john89ram@gmail.com>
2022-11-01 16:16:38 -0500

    Notes

--- Chapter7_UserInput_WhileLoop/lesson9_pets.py
+++ Chapter7_UserInput_WhileLoop/lesson9_pets.py
@@ -2,10 +2,13 @@
     # We are going to remove all instance of a specific value
 
 pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+# Here is a list of random pets that we have created.
 print(pets)
 
 while 'cat' in pets:
+    # While they are 'cat's inside the list the while loop stay active
     pets.remove('cat')
+    # Remove one 'cat' from the list and start over again
     
 print(pets)
 
