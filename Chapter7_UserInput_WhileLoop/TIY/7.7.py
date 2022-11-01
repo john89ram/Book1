73572b8557f2fac4085bce9f0e1713e527fd2101
john89ram <john89ram@gmail.com>
2022-11-01 14:51:41 -0500

    .

---
+++ Chapter7_UserInput_WhileLoop/TIY/7.7.py
@@ -0,0 +1,8 @@
+# Infinity - Create a While loop that will run infinitely 
+    # To close a program that is stuck in press the "Win Key" and "C" 
+
+count = 0
+
+while 1 == 1:
+    print(count)
+    count += 1
