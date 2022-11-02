1f6f71d3731f9657447600497a3085c716c01533
john89ram <john89ram@gmail.com>
2022-11-02 10:35:32 -0500

    Ch8 Lesson1

---
+++ Chapter8_Functions/lesson1_greeter.py
@@ -0,0 +1,15 @@
+# Lets practice creating a simple function.
+
+def greet_user():
+    # Here the "def" tells Python that we are defining a function called "greet_user"
+        # This is called a "function definition"
+    """Display a simple greeting."""
+    # ^ This is called a docstring, these describes the function
+        # Docstring are created by using 3 quotations """docstring"""
+    print("Hello!")
+    # After the formalities you can write the code for your function. 
+        # This can be any code you can write, this is just a simple "hello world" script.
+
+greet_user()
+# Now you can just call the function and Python will search for and run the function.
+    # Because we did not need input here it will just print.
