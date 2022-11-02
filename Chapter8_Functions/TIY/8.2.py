9c0703c5b1a7388b0f44775a7c787ebafef4ddf4
john89ram <john89ram@gmail.com>
2022-11-02 13:50:57 -0500

    TIY 8.2

---
+++ Chapter8_Functions/TIY/8.2.py
@@ -0,0 +1,9 @@
+# Favorite Book - Create a function called favorite_book() and have it
+    # accepts a parameter 'title'. The function should print a message 
+    # "My favorite book is {title}" Make sure the message is grammatically correct. 
+
+def favorite_book(title):
+    """This function will take the input title and print out the favorite book of the user."""
+    print(f"My favorite book is {title.title()}.")
+
+favorite_book("Enders game")
