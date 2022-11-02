fae2e506cc1470d35fd0b41571d54dec3d621993
john89ram <john89ram@gmail.com>
2022-11-02 13:34:13 -0500

    TIY 8.1

---
+++ Chapter8_Functions/TIY/8.1.py
@@ -0,0 +1,10 @@
+# Message - Create a function called displayed_message(), that prints
+    # one sentence telling everyone what you are learning about in this 
+    # chapter. 
+
+def displayed_message():
+    """Prints what we are covering this chapter."""
+    print("In this chapter, we will be going over how to create and work"
+    "with functions.")
+
+displayed_message()
