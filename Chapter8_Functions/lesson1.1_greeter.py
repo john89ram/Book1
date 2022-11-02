42bcd4bcf6069cdb73b6d55f9fb2ba92351ac0be
john89ram <john89ram@gmail.com>
2022-11-02 13:15:25 -0500

    Ch8 L1

---
+++ Chapter8_Functions/lesson1.1_greeter.py
@@ -0,0 +1,11 @@
+# Lets build on what we learned and build on top of it.
+    # Take the previous lesson and add an input inside the ()
+
+def greet_user(username):
+    #               ^ Here we define a variable called "username"
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
+    # and insert the variable named into our code.
+
+greet_user('jesse')
+# So jesse will = username and will be printed.
