80aa5ed1a36361aa3cc0cdb02e2215a584a7bdca
john89ram <john89ram@gmail.com>
2022-11-03 11:54:11 -0500

    Lesson2

---
+++ Chapter8_Functions/lesson2_pets.py
@@ -0,0 +1,10 @@
+# Positional Arguments
+    # Lets practice with multiple parameters and position
+
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet('hamster', 'harry')
+describe_pet('dog', 'willie')
