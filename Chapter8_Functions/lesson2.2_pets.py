82eb54ce4973f21e6bb305a552e3a0f1d0bf2532
john89ram <john89ram@gmail.com>
2022-11-08 07:59:52 -0600

    L2.2 Default values

---
+++ Chapter8_Functions/lesson2.2_pets.py
@@ -0,0 +1,12 @@
+# Default values - Functions can have default values set so no arguments need to be entered.
+    # They can be overwritten if enter the full keyword pair.
+
+def describe_pet(pet_name, animal_type='dog'):
+    # Be sure if a default value is used to put it in the end of the parameter list.
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet('Willie')
+describe_pet(pet_name='Harry')
+describe_pet(pet_name='Harry', animal_type='hamster')
