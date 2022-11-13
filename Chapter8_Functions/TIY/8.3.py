b9957b3d846595ecd28c157cc81de7d40d8fc900
john89ram <john89ram@gmail.com>
2022-11-13 12:28:05 -0600

    8.3 running thought these.

---
+++ Chapter8_Functions/TIY/8.3.py
@@ -0,0 +1,11 @@
+# T-shirt - Create a function called make_shirt() with the parameters 'size' and 'text'.
+    # The function should print a sentence that will overview the order of the shirt's size and
+    # the message ('text') that should be printed on the shirt.
+
+def make_shirt(size, text):
+    """The function should print a sentence that will overview the order of the shirt's size and,"""
+    """the message ('text') that should be printed on the shirt."""
+
+    print(f'\nYou have ordered a {size} shirt, with "{text}" printed.')
+
+make_shirt('small', 'The man /\ and the legend \/')
