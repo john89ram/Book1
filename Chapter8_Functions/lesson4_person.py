98343285034143477e3c1f7e7557b3fd14896903
john89ram <john89ram@gmail.com>
2022-11-13 18:29:51 -0600

    error

---
+++ Chapter8_Functions/lesson4_person.py
@@ -0,0 +1,10 @@
+# Returning a dictionary - functions can also store and return dictionary information.
+    # Lets create a simple function that explores this concept.
+
+def build_person(first_name, last_name):
+    """ This function will take basic information for a person"""
+    person = {'first': first_name, 'last': last_name}
+    return person
+
+musician = build_person('jimi', 'hendrix')
+print = musician
