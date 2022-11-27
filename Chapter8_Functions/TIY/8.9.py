a800217576bee695300d3355131cd86d25f0b51a
john89ram <john89ram@gmail.com>
2022-11-27 14:31:45 -0600

    re-edits

--- Chapter8_Functions/TIY/8.9.py
+++ Chapter8_Functions/TIY/8.9.py
@@ -1,19 +1,19 @@
 # Messages - Create a list of short messages called 'text_messages' and write a function called
     # 'show_messages', that will print each message.
 
-def archiving_messages(text_messages, message_history):
+def send_messages(text_messages, sent_messages):
     """
     This will take all messages that have been sent and archive them in the history folder.
     """
     while text_messages:
         old_messages = text_messages.pop()
-        message_history.append(old_messages)
+        sent_messages.append(old_messages)
 
-def show_messages(message_history):
+def show_messages(sent_messages):
     """
     Will show all text message history from newest to oldest
     """
-    for messages in message_history:
+    for messages in sent_messages:
         print(messages)
 
 
@@ -22,7 +22,7 @@ def show_messages(message_history):
 text_messages = ['Hey, babe it did it again.', 
 'Send this to Andrew!', 
 'Okay  update... clicked the I am here button. Curbside was an option, but I still have to sit and wait while they make it...',]
-message_history = []
+sent_messages = []
 
-archiving_messages(text_messages, message_history)
-show_messages(message_history)
+send_messages(text_messages, sent_messages)
+show_messages(sent_messages)
