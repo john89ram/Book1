4578b8b7ade8a5644fabeb42548de1ef94bb4c2c
john89ram <john89ram@gmail.com>
2022-12-19 13:26:35 -0600

    forgot to save

---
+++ Chapter8_Functions/lesson9_profile.py
@@ -0,0 +1,6 @@
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing everything we know about a user."""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    
+    
