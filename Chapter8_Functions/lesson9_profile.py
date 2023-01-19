3b4f4424ccd71a5d175c4deaf931e2ca1e381d75
Jonathan Ramriez <john89ram@gmail.com>
2023-01-19 13:50:32 -0600

    .

--- Chapter8_Functions/lesson9_profile.py
+++ Chapter8_Functions/lesson9_profile.py
@@ -1,4 +1,4 @@
-# Here we use the "**" to create an empty dictionary to accept any name-value pairs.
+# Here we use the "**" (aka "kwargs" - keyword arguments) to create an empty dictionary.
     # This is helpful when we are unsure to the amount of data that will be entered.
 def build_profile(first, last, **user_info):
     """Build a dictionary containing everything we know about a user."""
@@ -8,7 +8,7 @@ def build_profile(first, last, **user_info):
 
 user_profile = build_profile('albert', 'einstein', 
 # Here after the names parameters "first/last_names", the user added location and field.
-    # Similar to this example all that is needed is key(name)-value pair.
+    # Similar to this example all that is needed is the key-value pair.
                             location = "princeton",
                             field = "physics")
 
