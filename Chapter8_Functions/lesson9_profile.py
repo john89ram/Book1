5cd27eba50a9a57c138f777c559dfba4adcc7157
Jonathan Ramriez <john89ram@gmail.com>
2023-01-19 13:21:49 -0600

    Comments

--- Chapter8_Functions/lesson9_profile.py
+++ Chapter8_Functions/lesson9_profile.py
@@ -1,3 +1,5 @@
+# Here we use the "**" to create an empty dictionary to accept any keyword arguments.
+    # This is helpful when we are unsure to the amount of data that will be entered.
 def build_profile(first, last, **user_info):
     """Build a dictionary containing everything we know about a user."""
     user_info['first_name'] = first
@@ -5,6 +7,8 @@ def build_profile(first, last, **user_info):
     return user_info
 
 user_profile = build_profile('albert', 'einstein', 
+# Here after the names parameters "first/last_names", the user added location and field.
+    # Similar to this example all that is needed is the parameter and the data linked to it.
                             location = "princeton",
                             field = "physics")
 
