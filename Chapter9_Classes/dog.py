2d81d1e1706b034c3e550ec5bd43bcf93c666fa3
Jonathan Ramriez <john89ram@gmail.com>
2023-01-20 17:18:35 -0600

    Starting on explore classes

---
+++ Chapter9_Classes/dog.py
@@ -0,0 +1,15 @@
+class Dog:
+    """A simple attempt to model a dog."""
+
+    def __init__(self, name, age):
+        """Initialize name and age attributes"""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate a dog sitting in response to a command"""
+        print(f"{self.name} is now sitting.")
+
+    def roll_over(self):
+        """Simulate rolling over in response to a command"""
+        print(f"{self.name} rolled over!")
