2c25595d1e1c5a744ca6ca3f08e5a8ec7113f035
Jonathan Ramriez <john89ram@gmail.com>
2023-01-19 20:06:06 -0600

    Beginning with modules!

---
+++ Chapter8_Functions/modules/making_pizza.py
@@ -0,0 +1,6 @@
+# First we have to import our function from our neighboring file.
+import pizza
+
+# Next we call on it by name and call the function defined in it.
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushroom', 'green peppers', 'extra cheese')
