bdb3ddcb5cd030dd66073ac634ade8b169e9082e
Jonathan Ramriez <john89ram@gmail.com>
2023-01-24 12:37:17 -0600

    9.5 start

---
+++ Chapter9_Classes/TIY/9.5.py
@@ -0,0 +1,4 @@
+# Login Attempts - Copy the exercise from 9.3.
+    # Add an attribute called login_attempts
+    # Write a method called increment_login_attempts() that increases the value of login_attempts
+    # Write another method called reset_login_attempts() that resets the value
