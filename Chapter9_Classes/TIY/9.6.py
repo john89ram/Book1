8460db94df73191f78018bc85e8c1a898125842a
john89ram <john89ram@gmail.com>
2023-02-09 20:23:56 -0600

    Rewrite and assistance..

--- Chapter9_Classes/TIY/9.6.py
+++ Chapter9_Classes/TIY/9.6.py
@@ -4,51 +4,49 @@
         # Then create a method that displays these flavors. 
         # Lastly create an instance of IceCreamStand, and call the method.
 
-class Restaurant:
-    """ A simple class to create a restaurant and accepting the attributes name and cuisine type"""
+class Restaurant():
+    """A class representing a restaurant."""
 
-    def __init__(self, restaurant_name, cuisine_type):
-        """Initialize the attributes of the restaurant"""
-        self.name = restaurant_name
-        self.cuisine = cuisine_type
-        self.served = 0
+    def __init__(self, name, cuisine_type):
+        """Initialize the restaurant."""
+        self.name = name.title()
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
-        """Simple return of the attributes of the restaurant."""
-        print(f"Welcome to the {self.name}! We serve {self.cuisine} food.")
+        """Display a summary of the restaurant."""
+        msg = self.name + " serves wonderful " + self.cuisine_type + "."
+        print("\n" + msg)
 
     def open_restaurant(self):
-        """Simple message to indicate that a restaurant is open"""
-        print(f"The {self.name} is now open!")
+        """Display a message that the restaurant is open."""
+        msg = self.name + " is open. Come on in!"
+        print("\n" + msg)
 
-    def number_served(self):
-        """Prints the number of customers served"""
-        print(f"Congrats! We have served {self.served} customers.")
+    def set_number_served(self, number_served):
+        """Allow user to set the number of customers that have been served."""
+        self.number_served = number_served
 
-    def set_number_served(self, base_served):
-        """Setting the number of customers that have been served"""
-        if base_served >= self.served:
-            self.served = base_served
-        else:
-            print("Please do not enter a lower number of customers we have served.")
-
-    def increment_number_served(self, todays_customer_count):
-        """Simply adds todays number of customers served to the total of customers served"""
-        self.served += todays_customer_count
+    def increment_number_served(self, additional_served):
+        """Allow user to increment the number of customers served."""
+        self.number_served += additional_served
 
 class IceCreamStand(Restaurant):
-    """ A child class to simply explain a Ice Cream Stand """
+    """Represent an ice cream stand."""
 
-    def __init__(self, restaurant_name, cuisine_type, *flavors):
-        """Initialize the attributes of an Ice Cream Stand"""
-        super().__init__(restaurant_name, cuisine_type)
-        self.flavors = flavors
+    def __init__(self, name, cuisine_type='ice_cream'):
+        """Initialize an ice cream stand."""
+        super().__init__(name, cuisine_type)
+        self.flavors = []
 
-    def list_flavors(self):
-        """Print all flavors that """
-        print(self.flavors)
+    def show_flavors(self):
+        """Display the flavors available."""
+        print("\nWe have the following flavors available:")
+        for flavor in self.flavors:
+            print("- " + flavor.title())
 
-dj_ice_cream_shop = IceCreamStand("DJ's Ice Cream Shop", "desert" ,"vanilla", "chocolate", "strawberry", "mint")
-dj_ice_cream_shop.describe_restaurant()
-dj_ice_cream_shop.open_restaurant()
-dj_ice_cream_shop.list_flavors()
+big_one = IceCreamStand('The Big One')
+big_one.flavors = ['vanilla', 'chocolate', 'black cherry']
+
+big_one.describe_restaurant()
+big_one.show_flavors()
