3a498126ac310134532a7c0e65da0b706abfeaef
john89ram <john89ram@gmail.com>
2023-02-12 21:53:18 -0600

    9.14

---
+++ Chapter9_Classes/TIY/9.14.py
@@ -0,0 +1,20 @@
+# Lottery - Make a list or tuple containing a series of 10 numbers and five letters
+    # Randomly select four in the list and print a message reporting the winning char
+    
+from random import choice
+
+possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'a', 'b', 'c', 'd', 'e']
+
+winning_ticket = []
+print("Let's see what the winning ticket is...")
+
+# We don't want to repeat winning numbers or letters, so we'll use a
+#   while loop.
+while len(winning_ticket) < 4:
+    pulled_item = choice(possibilities)
+
+    # Only add the pulled item to the winning ticket if it hasn't
+    #   already been pulled.
+    if pulled_item not in winning_ticket:
+        print(f"  We pulled a {pulled_item}!")
+        winning_ticket.append(pulled_item)
