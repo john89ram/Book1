822b244922748470bce9222d9c4a043d2e52be4a
Jonathan Ramriez <john89ram@gmail.com>
2023-02-12 19:10:36 -0600

    import full module

---
+++ Chapter9_Classes/lesson4.3_full_import.py
@@ -0,0 +1,10 @@
+import lesson4_car
+# Be sure when you import the entire module that you use dot notation to call methods.
+    # Dot notation uses module_name.ClassName(), lets practice here.
+    # This can help with naming and less mess ups.
+
+my_beetle = lesson4_car.Car('volkswagen', 'beetle', 2019)
+print(my_beetle.get_descriptive_name())
+
+my_tesla = lesson4_car.Electric_car('tesla', 'roadster', 2019)
+print(my_tesla.get_descriptive_name())
