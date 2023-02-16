f757012a841e33674645c6c70b30f39a80116b1f
Jonathan Ramriez <john89ram@gmail.com>
2023-02-15 18:56:13 -0600

    .

--- Chapter10_Files_Exceptios/Ch1 Reading from a file/file_reader.py
+++
@@ -1,4 +0,0 @@
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-
-print(contents)
---
+++ Chapter10_Files_Exceptios/Ch1ReadingFromAFile/file_reader.py
@@ -0,0 +1,5 @@
+import os
+
+print(os.getcwd())
+os.chdir('C:\Users\"Jonathan Ramirez"\Project\Book1\Chapter10_Files_Exceptios\"Ch1 Reading from a file"')
+print(os.getcwd())
