8ddac45629771f9641b4953ecce03b430fee7737
Jonathan Ramriez <john89ram@gmail.com>
2023-02-15 19:10:51 -0600

    updates

--- Chapter10_Files_Exceptions/Ch1ReadingFromAFile/file_reader.py
+++ Chapter10_Files_Exceptions/Ch1ReadingFromAFile/file_reader.py
@@ -1,6 +1,13 @@
 # This only works if both files are in the same directory.
 
-with open('pi_digits.txt') as file_object:
+# Can be used path to locate file if Python evnr running in parent directory.
+with open('Chapter10_Files_Exceptions/Ch1ReadingFromAFile/pi_digits.txt') as file_object:
     contents = file_object.read()
 
 print(contents)
+
+# Script from book but will not run in my evnr.
+#with open('pi_digits.txt') as file_object:
+#    contents = file_object.read()
+#
+#print(contents)
