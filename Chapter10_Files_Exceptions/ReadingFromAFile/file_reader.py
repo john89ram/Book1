9171a32754cc0e52efc6e0c7d56c7411442c8279
Jonathan Ramriez <john89ram@gmail.com>
2023-02-24 23:42:48 -0600

    almost there

--- Chapter10_Files_Exceptions/ReadingFromAFile/file_reader.py
+++ Chapter10_Files_Exceptions/ReadingFromAFile/file_reader.py
@@ -39,7 +39,7 @@ with open(file_path) as file_object:
     lines = file_object.readlines()
 
 # We just assigned the readlines output to the "lines" variable.
-    # Now we can just print it whenever we might need it.
+    # Now we can just print it whenever we might need it (saving the txt file into memory).
 for line in lines:
     print(f"\n{line.rstrip()}")
 
@@ -47,3 +47,59 @@ for line in lines:
 
 ### Working with a file's contents ###
 
+# Now lets work with the txt file now that it is saved into memory. 
+    # Lets put it into a single line with no breaks
+
+pi_string = ''
+# Here we set pi_string as an empty variable, this is a place holder so we can transfer the data in
+    # lines (pi_digits.txt) into pi_string
+for line in lines:
+    pi_string += line.rstrip()
+    # Now with each line we are stripping the '\n' that readlines() hides while operating
+        # giving us just the tabs only that we created formatting the txt file 
+print(pi_string)
+print(len(pi_string))
+
+# Lets continue by just using the strip() method and get rid of all whitespace
+    # Lets use another place holder called pi_string2
+        # We can still use the lines variable bc we did not alter the data, hence the empty variable
+pi_string2 = ''
+
+for line in lines:
+    pi_string2 += line.strip()
+
+print(pi_string2)
+print(len(pi_string2))
+# Now there is no space and pi is back together, we have len() to confirm this as well
+
+#---------------------------------------------------------------------------------------------------
+
+### Large files: One million digits ###
+
+filename = 'Chapter10_Files_Exceptions/ReadingFromAFile/pi_million_digits.txt'
+
+with open(filename) as file_object:
+    mil_lines = file_object.readlines()
+
+pi_string_mil = ''
+
+for mil_line in mil_lines:
+    pi_string_mil += mil_line.strip()
+
+print(f"{pi_string_mil[:52]}...")
+print(len(pi_string_mil))
+
+#---------------------------------------------------------------------------------------------------
+
+### Is your birthday contained in pi ###
+
+# Now lets play with the data and see if we can create a script to search for a birthday in pi
+
+# Simple user input requesting 6 digits and assigning them to "birthday"
+birthday = input(f"\nEnter your birthday, in the form mmddyy: ")
+
+# Now an easy if statement to see if the 6 character string is anywhere in the pi_million txt file
+if birthday in pi_string_mil:
+    print(f"Your birthday appears in the first million digits of pi!")
+else:
+    print(f"You birthday does not appear in the first million digits of pi.")
