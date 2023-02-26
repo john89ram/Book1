35a8576bc139f497e0928d61402a08deabb9b2f9
Jonathan Ramriez <john89ram@gmail.com>
2023-02-26 17:25:32 -0600

    Analyzing text files

--- Chapter10_Files_Exceptions/3Exceptions/alice.py
+++ Chapter10_Files_Exceptions/3Exceptions/alice.py
@@ -20,3 +20,24 @@ try:
         contents = f.read()
 except FileNotFoundError:
     print(f"Sorry, the file {filename} does not exist.")
+
+#---------------------------------------------------------------------------------------------------
+
+### Analyzing text ### 
+
+# Now lets create a program to analyze a txt file. 
+# For this project lets simply count how many words within the play "Alice In Wonderland"
+# The txt file was copied from: https://www.gutenberg.org/cache/epub/35688/pg35688.txt
+
+alice_txt = "Chapter10_Files_Exceptions/3Exceptions/Alice_In_Wonderland.txt"
+
+try:
+    with open(alice_txt, encoding='utf-8') as f_alice:
+        content_alice = f_alice.read()
+except FileNotFoundError:
+    print(f"Sorry, the file {alice_txt} does not exist.")
+else:
+    # Count the number of words in the file.
+    words = content_alice.split()
+    num_words = len(words)
+    print(f"\n{alice_txt} has about {num_words} words.\n")
