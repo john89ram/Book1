e3ad97bd9f40fd81e6c9cb89b94692b3df5750ca
Jonathan Ramriez <john89ram@gmail.com>
2023-02-27 16:17:01 -0600

    .

--- Chapter11_Testing_Your_Code/1TestAFunction/name_function.py
+++ Chapter11_Testing_Your_Code/1TestAFunction/name_function.py
@@ -1,9 +1,23 @@
+# First draft of function.
 def get_formatted_name(first, last):
     """Generate a neatly formatted full name."""
     full_name = f"{first} {last}"
     return full_name.title()
 
+# Revised function to add middle names as well
 def get_formatted_name2(first, middle, last):
     """Generate a neatly formatted full name."""
     full_name = f"{first} {middle} {last}"
+    return full_name.title()
+
+# Another revision to correct failed test
+def get_formatted_name3(first, last, middle=''):
+    # Set the middle argument to nothing unless specified.
+    """Generate a neatly formatted full name."""
+    # Now if a middle name was added it will follow this if block
+    if middle:
+        full_name = f"{first} {middle} {last}"
+    # of else it will remain blank and only first and last names will be counted for
+    else:
+        full_name = f"{first} {last}"
     return full_name.title()
--- Chapter11_Testing_Your_Code/1TestAFunction/test_name_function_fail.py
+++ Chapter11_Testing_Your_Code/1TestAFunction/test_name_function_fail.py
@@ -4,17 +4,60 @@
 
 import unittest
 from name_function import get_formatted_name2
+from name_function import get_formatted_name3
 
+#class NamesTestCase(unittest.TestCase):
+#    """Tests for 'name_function.py'."""
+#    
+#    def test_first_last_name(self):
+#        """Do names like 'Janis Joplin' work?"""
+#        # Since our test only has two names entered this test should fail as there is no 
+#            # 3 argument {last}.
+#        formatted_name = get_formatted_name2('janis', 'joplin')
+#        self.assertEqual(formatted_name, 'Janis Joplin')
+#
+#if __name__ == '__main__':
+#    unittest.main()
+
+#---------------------------------------------------------------------------------------------------
+
+### Responding to a failed test ###
+    
+    # When trying to decided what to do with a failed test, it is important to know that best practice is to not change how the test functions but augment the function itself to work the way you need and still pass. 
+
+    # Lets redesign get_formatted_name to allow first and last names as well
+
+# Test to see if our revisions worked. 
+#class NamesTestCase(unittest.TestCase):
+#    """Tests for 'name_function.py'."""
+#    
+#    def test_first_last_name(self):
+#        """Do names like 'Janis Joplin' work?"""
+#        formatted_name = get_formatted_name3('janis', 'joplin')
+#        self.assertEqual(formatted_name, 'Janis Joplin')
+#
+#if __name__ == '__main__':
+#    unittest.main()
+
+#---------------------------------------------------------------------------------------------------
+
+### Adding new tests ###
+
+    # Now lets include another test to see if our function works properly with middle names added.
+
+# Test to see if last names will also be formatted as well. 
 class NamesTestCase(unittest.TestCase):
     """Tests for 'name_function.py'."""
     
     def test_first_last_name(self):
         """Do names like 'Janis Joplin' work?"""
-        # Since our test only has two names entered this test should fail as there is no 
-            # 3 argument {last}.
-        formatted_name = get_formatted_name2('janis', 'joplin')
+        formatted_name = get_formatted_name3('janis', 'joplin')
         self.assertEqual(formatted_name, 'Janis Joplin')
+
+    def test_first_last_middle_name(self):
+        """Do names like 'Wolfgang Amadeus Mozart' work?"""
+        formatted_name = get_formatted_name3('wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
 
 if __name__ == '__main__':
     unittest.main()
-
