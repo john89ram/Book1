243c6672c0b32fb70e539c8cd7d044ea41473bf2
Jonathan Ramriez <john89ram@gmail.com>
2023-02-27 15:51:43 -0600

    .

--- Chapter11_Testing_Your_Code/1TestAFunction/name_function.py
+++ Chapter11_Testing_Your_Code/1TestAFunction/name_function.py
@@ -2,3 +2,8 @@ def get_formatted_name(first, last):
     """Generate a neatly formatted full name."""
     full_name = f"{first} {last}"
     return full_name.title()
+
+def get_formatted_name2(first, middle, last):
+    """Generate a neatly formatted full name."""
+    full_name = f"{first} {middle} {last}"
+    return full_name.title()
---
+++ Chapter11_Testing_Your_Code/1TestAFunction/test_name_function_pass.py
@@ -0,0 +1,15 @@
+### A passing test ###
+
+import unittest
+from name_function import get_formatted_name
+
+class NamesTestCase(unittest.TestCase):
+    """Tests for 'name_function.py'."""
+    
+    def test_first_last_name(self):
+        """Do names like 'Janis Joplin' work?"""
+        formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, 'Janis Joplin')
+
+if __name__ == '__main__':
+    unittest.main()
