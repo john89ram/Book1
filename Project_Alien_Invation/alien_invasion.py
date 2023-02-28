cbf633e52ae6ac55bb404babae41c41e3d5bc55d
Jonathan Ramriez <john89ram@gmail.com>
2023-02-28 09:40:38 -0600

    continuing

--- Project_Alien_Invation/alien_invasion.py
+++ Project_Alien_Invation/alien_invasion.py
@@ -24,17 +24,26 @@ class AlienInvasion:
     def run_game(self):
         """Start the mail loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+    def _check_events(self):
+        """Respond to keypress and mouse events."""
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen, flip to the new screen."""
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
