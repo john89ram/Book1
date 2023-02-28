58cff1fe39b9fbb39f648efd3917e938de1c42d0
Jonathan Ramriez <john89ram@gmail.com>
2023-02-28 08:24:29 -0600

    .

--- Project_Alien_Invation/alien_invasion.py
+++ Project_Alien_Invation/alien_invasion.py
@@ -1,5 +1,10 @@
 import sys
 import pygame
+from settings import Settings
+
+#------------------------------------------------------------------------------------------------------
+
+### Creating a pygame window and responding to user input ###
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -7,8 +12,10 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                                self.settings.screen_height))
         pygame.display.set_caption(" Alien Invasion")
 
     def run_game(self):
@@ -18,6 +25,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
