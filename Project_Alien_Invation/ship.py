e40d6ec248738bd3ac2b070cf626ec8f282e701e
Jonathan Ramriez <john89ram@gmail.com>
2023-02-28 11:05:17 -0600

    moving right

--- Project_Alien_Invation/ship.py
+++ Project_Alien_Invation/ship.py
@@ -15,6 +15,14 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Movement flag
+        self.moving_right = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x += 1
+
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
