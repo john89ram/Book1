ba914984f4327447bb7aae25a7b171851299b1f5
Jonathan Ramriez <john89ram@gmail.com>
2023-02-28 15:27:35 -0600

    program to have the game run in full screen mode

--- Project_Alien_Invation/alien_invasion.py
+++ Project_Alien_Invation/alien_invasion.py
@@ -14,10 +14,14 @@ class AlienInvasion:
         """Initialize the game, and create game resources"""
         pygame.init()
         self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                                self.settings.screen_height))
+        
+        # Enable fullscreen mode
+        # self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
 
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption(" Alien Invasion")
 
         # Draw ship to the screen
@@ -55,7 +59,7 @@ class AlienInvasion:
             # move the ship to the left
             self.ship.moving_left = True
         
-        # press 'q' to quit the game
+        # press 'q' to quit the game.
         elif event.key == pygame.K_q:
             sys.exit()
 
