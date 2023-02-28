30ab782d2a3a13c59b5692060ae3cb337b86fde9
Jonathan Ramriez <john89ram@gmail.com>
2023-02-28 12:27:03 -0600

    ship speed completed

--- Project_Alien_Invation/settings.py
+++ Project_Alien_Invation/settings.py
@@ -9,6 +9,11 @@ class Settings:
 
     def __init__(self):
         """Initialize the game's settings."""
+
+        # Game window settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
+
+        # Ship settings
+        self.ship_speed = 1.5
--- Project_Alien_Invation/ship.py
+++ Project_Alien_Invation/ship.py
@@ -7,6 +7,7 @@ class Ship:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
@@ -15,6 +16,9 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+
         # Movement flag
         self.moving_right = False
         self.moving_left = False
@@ -22,10 +26,14 @@ class Ship:
 
     def update(self):
         """Update the ship's position based on the movement flag."""
+        # Update the ship's x value, not the rect.
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x 
 
 
     def blitme(self):
