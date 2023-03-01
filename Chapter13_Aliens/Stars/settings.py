42c2afe12ca7ba3a65cc29a992df480091ef1ec7
Jonathan Ramriez <john89ram@gmail.com>
2023-03-01 15:11:29 -0600

    13.1 complete

---
+++ Chapter13_Aliens/Stars/settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """blah bola blah"""
+
+    def __init__(self):
+        """You know"""
+        self.screen_width = 1200
+        self.screen_height = 1200
+        self.bg_color = (0,0,0)
+
+            
---
+++ Chapter13_Aliens/Stars/star.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.sprite import Sprite
+ 
+class Star(Sprite):
+    """A class to represent a single star."""
+
+    def __init__(self, sn_grid):
+        """Initialize the star and set its starting position."""
+        super().__init__()
+        self.screen = sn_grid.screen
+
+        # Load the star image and set its rect attribute.
+        self.image = pygame.image.load('Chapter13_Aliens/Stars/star.png')
+        self.rect = self.image.get_rect()
+
+        # Start each new star near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store the star's exact vertical position.
+        self.y = float(self.rect.y)
