c582a8d35aa908720871f0aa7873eba0b5ed5e77
Jonathan Ramriez <john89ram@gmail.com>
2023-03-01 15:24:03 -0600

    13.2 completed

---
+++ Chapter13_Aliens/Stars_Random/starry_night.py
@@ -0,0 +1,67 @@
+import sys
+import pygame
+from settings import Settings
+from star import Star
+from random import randint
+
+class StarryNight:
+    """it says it up there"""
+
+    def __init__(self):
+        """a;lskdjf"""
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                              self.settings.screen_height))
+        
+        self.star = pygame.sprite.Group()
+        self._create_grid()
+
+
+    def run(self):
+        """a;lsdkfja;lskdjfajng"""
+        while True:
+            self._events()
+            self._update_screen()
+
+    def _events(self):
+        """asd;lfkja;dslkvnapruiohv;afdnv"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _create_grid(self):
+        """yeah yeah yeah"""
+        star = Star(self)
+        star_width, star_height = star.rect.size
+        
+        available_x = self.settings.screen_width
+        number_stars_x = available_x//(star_width)
+        
+        available_y = self.settings.screen_height
+        number_rows = available_y // star_height
+
+        for row_number in range(number_rows):
+            for star_number in range(number_stars_x):
+                self._create_star(star_number, row_number)
+
+    def _create_star(self, star_number, row_number):
+        """almost there!!!"""
+        star = Star(self)
+        star_width, star_height = star.rect.size
+        star.x = star_width * star_number
+        star.rect.x = randint(0, self.settings.screen_width)
+        star.rect.y = randint(0, self.settings.screen_height)
+        self.star.add(star)
+
+    def _update_screen(self):
+        """OMG this is not fun doing it fast"""
+        self.screen.fill(self.settings.bg_color)
+        self.star.draw(self.screen)
+
+        pygame.display.flip()
+
+
+if __name__=="__main__":
+    sn = StarryNight()
+    sn.run()
