1bf55c7187293196adb4d758aaf34f4ddd9e5136
john89ram <john89ram@gmail.com>
2023-02-28 21:58:52 -0600

    Sideshooter game

---
+++ Chapter12_Alien_Invasion/TIY/sideshooter.py
@@ -0,0 +1,6 @@
+# 12-6 Sideways Shooter
+    # Write a game that places a ship on the left side of the screen
+    # The player is allowed to move up and down
+    # Allow the player to shoot bullets to the right of the screen.
+        # Delete the bullets when they disappear off the screen.
+        
