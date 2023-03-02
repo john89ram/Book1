8d323c4cd827ed275b8e682845ea049b31822622
Jonathan Ramriez <john89ram@gmail.com>
2023-03-02 10:32:01 -0600

    Needed GPT help as I was over complicating it

---
+++ Chapter13_Aliens/Rainstorm/raindrops.py
@@ -0,0 +1,70 @@
+# 13-3 Raindrops: 
+    # Program to handle all assets and places them on scene and main loop
+        # A row of raindrops needs to be created and at the top of the screen
+        # Once on screen they need to drop.
+        # End program
+import sys
+import pygame
+from settings import Settings
+from drop import Drop
+
+class Raindrops:
+    """OVerall class to manage scene assets and behavior"""
+
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                              self.settings.screen_height))
+        
+        self.drop = pygame.sprite.Group()
+        self._create_rain()
+
+    def run_scene(self):
+        """Main animation loop"""
+        while True:
+            self._update_events()
+            self._update_drops()
+            self._update_screen()
+
+    def _update_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _create_rain(self):
+        drop = Drop(self)
+        drop_w, drop_h = drop.rect.size
+
+        available_x = self.settings.screen_width
+        num_drops_x = available_x//drop_w
+
+        for drop_num in range(num_drops_x):
+            self._create_drop(drop_num)
+
+    def _create_drop(self, drop_num):
+        drop = Drop(self)
+        drop_w, drop_h = drop.rect.size
+        drop.x = drop_w*drop_num
+        drop.rect.x = drop.x
+        self.drop.add(drop)
+
+    def _update_drops(self):
+        self.drop.update()
+        for drop in self.drop.copy():
+            if drop.rect.top >= self.settings.screen_height:
+                self.drop.remove(drop)
+        if not self.drop:
+            self._create_rain()
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.drop.draw(self.screen)
+
+
+        pygame.display.flip()
+
+
+if __name__=="__main__":
+    rd = Raindrops()
+    rd.run_scene()
---
+++ Chapter13_Aliens/Rainstorm/settings.py
@@ -0,0 +1,13 @@
+
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+
+        # Game window settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (36,64,92)
+
+        self.drop_speed = 1
