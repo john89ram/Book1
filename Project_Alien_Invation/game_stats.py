f82753fc3dd7ee838681d27ddfe9dab83e97bb19
Jonathan Ramriez <john89ram@gmail.com>
2023-03-03 11:18:05 -0600

    .

--- Project_Alien_Invation/game_stats.py
+++ Project_Alien_Invation/game_stats.py
@@ -6,7 +6,7 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
         # Start Alien Invasion in an active state
-        self.game_active = True
+        self.game_active = False
 
     def reset_stats(self):
         """Initialize statistics that can change during the game"""
