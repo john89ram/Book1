d5958da89292e2df115ecc4201a9c56d4769a6b8
john89ram <john89ram@gmail.com>
2023-03-07 10:57:43 -0600

    Random data visualization

---
+++ Project2_Data_Visualizztion/random_walk_visual.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+
+# Keep making new walks, as long as the program is active
+while True:
+    # Create an instance of RandomWalk
+    rw = RandomWalk(50000)
+    rw.fill_walk()
+
+    # Plot the point in the walk 
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(15,9), dpi=128)
+    
+    #ax.scatter(rw.x_values, rw.y_values, s=15)
+    
+    # Stylizing the walk to see important patterns
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, 
+               edgecolors='none', s=15)
+    # Emphasize the first and last points
+    ax.scatter(0,0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red',
+               edgecolors='none', s=100)
+    
+    # Remove the axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
