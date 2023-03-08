cf575df9a996061e71592af467154a4c390ac9fd
john89ram <john89ram@gmail.com>
2023-03-08 10:33:06 -0600

    .

--- Project2_Data_Visualizztion/Chapter16_Download_Data/death_valley_highs_lows.py
+++ Project2_Data_Visualizztion/Chapter16_Download_Data/death_valley_highs_lows.py
@@ -12,11 +12,15 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = int(row[5])
-        low = int(row[6])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # Plot the high temps
 plt.style.use('seaborn')
@@ -26,7 +30,8 @@ ax.plot(dates, lows, c='blue', alpha=0.5)
 ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot
-ax.set_title('Daily high and low temps - 2018', fontsize=24)
+title = f"Daily high and low temps - 2018\nDeath Valley, CA"
+ax.set_title(title, fontsize=20)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel('Temperature (F)', fontsize=16)
