215bbf5dc17f4a8f2a5de50b6a354f16be70d6b0
john89ram <john89ram@gmail.com>
2023-03-09 16:22:28 -0600

    .

--- Project2_Data_Visualizztion/Chapter17_Working_APIs/python_repos.py
+++ Project2_Data_Visualizztion/Chapter17_Working_APIs/python_repos.py
@@ -16,18 +16,20 @@ print(f"Repositories returned: {len(repo_dicts)}")
 
 # Examine the first repository 
 repo_dict = repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")
-for key in sorted(repo_dict.keys()):
-    print(key)
+#print(f"\nKeys: {len(repo_dict)}")
+#for key in sorted(repo_dict.keys()):
+#    print(key)
 
-print("\nSelected information about the repository")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    #print("\nSelected information about the repository")
+    print(f"\nName: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
 
 
 # Process results
