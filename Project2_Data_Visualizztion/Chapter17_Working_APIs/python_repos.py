38a761bfd0e0576b9f0b2b4580e209cc488d3850
john89ram <john89ram@gmail.com>
2023-03-09 15:34:55 -0600

    Need to continue Chapter17_Working_APIs

---
+++ Project2_Data_Visualizztion/Chapter17_Working_APIs/python_repos.py
@@ -0,0 +1,34 @@
+import requests
+
+#Make an API call and store the response
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application.vnd.guthub.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Store API response in a variable
+response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
+
+# Explore information about the repositories
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# Examine the first repository 
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
+
+print("\nSelected information about the repository")
+print(f"Name: {repo_dict['name']}")
+print(f"Owner: {repo_dict['owner']['login']}")
+print(f"Stars: {repo_dict['stargazers_count']}")
+print(f"Repository: {repo_dict['html_url']}")
+print(f"Created: {repo_dict['created_at']}")
+print(f"Updated: {repo_dict['updated_at']}")
+print(f"Description: {repo_dict['description']}")
+
+
+# Process results
+print(response_dict.keys())
